23d960eda617d42270c01b3a4675fb0355e9c27c
GIOVANA BARBOSA SOUZA <25133664@cfp127aluno.local>
2025-02-24 17:02:01 -0300

    começamos os getters e setters

--- src/br/senai/sp/jandira/clinica/ClinicaApp.java
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -10,23 +10,27 @@ public class ClinicaApp {
 		
 		// Criar um objeto do tipo paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Matheus";
+		p1.setNome("matheus"); 
 		p1.cpf = "999.888.777-66";
-		p1.altura = 1.75;
-		p1.peso = 75.000;
+		p1.setAltura(1.75);
+		p1.setPeso(75.0); 
 		p1.telefone = "(11)96523-8888";
 		p1.dataNascimento = LocalDate.of(2008, 7, 03);
 
 		Paciente p2 = new Paciente();
-		p2.nome = "Giovana";
+		p2.setNome("giovana");
 		p2.cpf = "581.280.238.12";
-		p2.altura = 1.65;
-		p2.peso = 52.000;
+		p2.setAltura(1.65);
+		p2.setPeso(52.0);
 		p2.telefone = "(11)962051962";
 		p2.dataNascimento = LocalDate.of(2005, 3, 15);
 		
 		p1.mostrarDados();
 		p2.mostrarDados();
+		
+		String teste = p1.getNome()
+		System.out.println(teste.toLowerCase());
+		System.out.println()
 
 		
 				
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -4,22 +4,52 @@ import java.time.LocalDate;
 
 public class Paciente {
 
-	public String nome;
+	private String nome;
 	public String cpf;
-	public double altura;
-	public double peso;
+	private double altura;
+	private double peso;
 	public String telefone;
 	public LocalDate dataNascimento;
-	
+
+	public void setAltura(double altura) {
+
+		if (altura > 0 && altura < 3) {
+			// TRUE
+			this.altura = altura;
+		} else {
+			// FALSE
+			System.out.println("A altura do(a) paciente" + nome + " deve ser maior que ZERO e menor do que TRÊS!");
+		}
+	}
+
+	public void setPeso(double peso) {
+		if (peso > 30 && peso < 500) {
+			// TRUE
+			this.peso = peso;
+		} else {
+			// FALSE
+			System.out.println("O peso do(a) paciente" + nome + " deve ser maior que 30 e menor que 500! ");
+		}
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+	}
+
+	public String getNome() {
+		return "Cheguei Brasil";
+
+	}
+
 	public void mostrarDados() {
 		System.out.println("-----------------");
 		System.out.println("DADOS DO PACIENTE");
 		System.out.println("-----------------");
-		System.out.println("Nome:"+ nome);
-		System.out.println("Peso:"+ peso + " kgs");
+		System.out.println("Nome:" + nome);
+		System.out.println("Peso:" + peso + " kgs");
 		System.out.println("Altura:" + altura + "m");
 		System.out.println("-----------------");
 		System.out.println();
 	}
-	
-	}
+
+}
