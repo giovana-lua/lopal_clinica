25a71758c2b94fe86ed8a9b4ba2307c3694e5609
GIOVANA BARBOSA SOUZA <25133664@cfp127aluno.local>
2025-02-24 13:36:29 -0300

    Inicio da construção da classe Paciente

---
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -0,0 +1,38 @@
+package br.senai.sp.jandira.clinica;
+
+import java.time.LocalDate;
+
+import br.senai.sp.jandira.clinica.model.Paciente;
+
+public class ClinicaApp {
+
+	public static void main(String[] args) {
+		
+		// Criar um objeto do tipo paciente
+		Paciente p1 = new Paciente();
+		p1.nome = "Matheus";
+		p1.cpf = "999.888.777-66";
+		p1.altura = 1.75;
+		p1.peso = 75.000;
+		p1.telefone = "(11)96523-8888";
+		p1.dataNascimento = LocalDate.of(2008, 7, 03);
+
+		Paciente p2 = new Paciente();
+		p2.nome = "Giovana";
+		p2.cpf = "581.280.238.12";
+		p2.altura = 1.65;
+		p2.peso = 52.000;
+		p2.telefone = "(11)962051962";
+		p2.dataNascimento = LocalDate.of(2005, 3, 15);
+		
+		p1.mostrarDados();
+		p2.mostrarDados();
+
+		
+				
+		
+		
+        
+	}
+
+}
